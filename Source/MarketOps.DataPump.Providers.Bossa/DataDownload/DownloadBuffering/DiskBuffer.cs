67a1bbf42cb51be236d12ee1cd18a1d6e784b562
minusminus <minusminus@tlen.pl>
2023-09-28 21:27:30 +0200

    - DiskBuffer with mutual exclusion on daily files buffer access

--- Source/MarketOps.DataPump.Providers.Bossa/DataDownload/DownloadBuffering/DiskBuffer.cs
+++ Source/MarketOps.DataPump.Providers.Bossa/DataDownload/DownloadBuffering/DiskBuffer.cs
@@ -5,12 +5,17 @@ namespace MarketOps.DataPump.Providers.Bossa.DataDownload.DownloadBuffering;
 
 /// <summary>
 /// Buffers downloaded files on disk.
+/// 
+/// Has simple locking on buffer dictionary access, so can be instantiated as a singleton.
+/// This simple lock prevents parallel downloading of the same file, but does not allow to download different files in parallel.
+/// For daily data with one file for one stock type it should be good enough.
 /// </summary>
 internal class DiskBuffer : IDownloadBuffer
 {
     private readonly IBossaDownloader _downloader;
     private readonly string _downloadPath = Path.Combine(Consts.ExecutingLocation, nameof(DiskBuffer));
 
+    private readonly object _dailyBufferLock = new();
     private readonly Dictionary<StockType, string> _dailyFilesBuffer = new();
 
     public DiskBuffer(IBossaDownloader downloader)
@@ -29,10 +34,19 @@ internal class DiskBuffer : IDownloadBuffer
 
     private BufferEntry GetDailyFile(StockDefinitionShort stockDefinition)
     {
-        if (!GetFromDailyBuffer(stockDefinition.Type, out string zipFilePath))
-            zipFilePath = DownloadAndStoreInDailyBuffer(stockDefinition);
-
+        string zipFilePath = GetDailyZipFilePath(stockDefinition);
         return GetDataFromZip(zipFilePath, $"{stockDefinition.Name}.mst");
+    }
+
+    private string GetDailyZipFilePath(StockDefinitionShort stockDefinition)
+    {
+        string zipFilePath;
+        lock (_dailyBufferLock)
+        {
+            if (!GetFromDailyBuffer(stockDefinition.Type, out zipFilePath))
+                zipFilePath = DownloadAndStoreInDailyBuffer(stockDefinition);
+        }
+        return zipFilePath;
     }
 
     private bool GetFromDailyBuffer(StockType stockType, out string zipFilePath) => 
