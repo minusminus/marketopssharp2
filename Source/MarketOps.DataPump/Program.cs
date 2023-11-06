3ba274714aea9b34447c03ce9aa3edf12b88a26e
minusminus <minusminus@tlen.pl>
2023-11-06 13:09:38 +0100

    - DataPump cli definition tests

--- Source/MarketOps.DataPump/Program.cs
+++ Source/MarketOps.DataPump/Program.cs
@@ -1,7 +1,9 @@
-﻿using MarketOps.DataPump.Common;
+﻿using MarketOps.DataPump.CliCommands;
+using MarketOps.DataPump.Common;
 using MarketOps.DataPump.SetUp;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
+using System.CommandLine;
 
 namespace MarketOps.DataPump;
 
@@ -15,11 +17,14 @@ internal class Program
             .ConfigureServices((context, services) => services.RegisterServices(context.Configuration))
             .Build();
 
-        var mainTask = host.RunAsync();
+        //var mainTask = host.RunAsync();
 
-        var executor = host.Services.GetRequiredService<IDataPumpExecutor>();
-        executor.Execute(Types.StockType.IndexFuture);
+        //var executor = host.Services.GetRequiredService<IDataPumpExecutor>();
+        //executor.Execute(Types.StockType.IndexFuture);
 
-        await mainTask;
+        //await mainTask;
+
+        await DefineCommands.Define()
+            .InvokeAsync(args);
     }
 }
