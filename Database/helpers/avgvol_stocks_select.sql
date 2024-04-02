10635b9f06bd777a2cbb7c135ef017ca6391ceb5
minusminus <minusminus@tlen.pl>
2024-04-02 21:33:02 +0200

    - avgvol

--- Database/helpers/avgvol_stocks_select.sql
+++ Database/helpers/avgvol_stocks_select.sql
@@ -1,12 +1,13 @@
 select *
 from 
 (
-select s.id, s.stock_name, avg(d.volume * d.close) as "avgvol"
+select s.id, s.stock_name, avg(d.volume * d.close) as "avgvol", min(d.volume * d.close) as "minvol"
 from at_spolki2 s, at_dzienne0 d
 where s.stock_type=0 and s.enabled=true
 and s.id=d.fk_id_spolki
-and d.ts > '2021-09-01'
+and d.ts > '2024-01-01'
 group by s.id, s.stock_name
 ) as t
 where t.avgvol >= 500000
+and t.minvol >= 150000
 order by t.avgvol desc
